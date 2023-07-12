8c83c80ec74f9f9e084c0893824f226ad81e863d
inbarasananbu <inbaanbu1998@gmail.com>
2023-07-12 22:43:59 +0530

    first commit

---
+++ src/main/java/dockerjenkintegrstion/dockerjenkintegrstion/DockerjenkintegrstionApplication.java
@@ -0,0 +1,13 @@
+package dockerjenkintegrstion.dockerjenkintegrstion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerjenkintegrstionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerjenkintegrstionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dockerjenkintegrstion/dockerjenkintegrstion/DockerjenkintegrstionApplicationTests.java
@@ -0,0 +1,13 @@
+package dockerjenkintegrstion.dockerjenkintegrstion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerjenkintegrstionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
